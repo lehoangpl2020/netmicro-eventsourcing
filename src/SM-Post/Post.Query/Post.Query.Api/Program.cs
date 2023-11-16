8136143ec2ce12ad6f983ae7ff74b5818936c267
lhpl <lhpl@LHPL.com>
2023-11-16 20:41:59 +0700

    Event consumer

--- src/SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ src/SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,8 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Post.Query.Infrastructure.Consumers;
+using Post.Query.Infrastructure.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+Action<DbContextOptionsBuilder> configureDbContext = o => o.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")); ;
+var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+builder.Services.AddDbContext<DatabaseContext>(configureDbContext);
+builder.Services.AddSingleton<DatabaseContextFactory>(new DatabaseContextFactory(configureDbContext));
+
+// create database and tables
+var dataContext = builder.Services.BuildServiceProvider().GetRequiredService<DatabaseContext>();
+dataContext.Database.EnsureCreated();
+
+
+
+
+
+
 
 builder.Services.AddControllers();
+
+
+
+builder.Services.AddHostedService<ConsumerHostedService>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
